ee850382066abf01d452e7076d3c73fcb8ddc713
Irfan Syarifudin <irfansyarifudin@students.amikom.ac.id>
2026-07-17 10:15:00 +0700

    Tambah konfigurasi aplikasi dan variabel lingkungan

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'kunci-rahasia-sistem-warnet-oop-2026'
+
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
+    MYSQL_PORT = int(os.environ.get('MYSQL_PORT') or 3306)
+    MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ''
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'db_warnet'

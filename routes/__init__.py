122afd4c9d7612f12c618cbd1d9560a8c0d28944
Irfan Syarifudin <irfansyarifudin@students.amikom.ac.id>
2026-07-20 09:00:00 +0700

    Buat paket modul routes

---
+++ routes/__init__.py
@@ -0,0 +1,7 @@
+from routes.dashboard import dashboard_bp
+from routes.komputer import komputer_bp
+from routes.pelanggan import pelanggan_bp
+from routes.penyewaan import penyewaan_bp
+from routes.transaksi import transaksi_bp
+from routes.laporan import laporan_bp
+from routes.log import log_bp
